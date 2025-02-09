18139297ca889b19cc3f85c006479eb286f7d764
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-02-09 19:06:48 +0100

    tui

---
+++ design-2/src/main.rs
@@ -0,0 +1,24 @@
+pub enum AST {
+    Integer(u64),
+    Double(f64),
+    Add(Vec<AST>),
+    Multiply(Vec<AST>)
+}
+use crossterm::event::{self, Event};
+use ratatui::{text::Text, Frame};
+
+fn main() {
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw).expect("failed to draw frame");
+        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
+            break;
+        }
+    }
+    ratatui::restore();
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello World!");
+    frame.render_widget(text, frame.area());
+}
