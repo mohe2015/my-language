0c3066750ee4ad1d6ef83b7a1cd723f139b2a4a1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-02-16 15:23:46 +0100

    wip

--- design-2/src/main.rs
+++ design-2/src/main.rs
@@ -43,33 +43,55 @@ impl CursorAST {
     }
 }
 
-pub fn ui(frame: &mut Frame) {
-    let ast = CursorAST {
-        cursor: false,
-        inner: CursorASTInner::Add(vec![CursorAST {
-            cursor: true,
-            inner: CursorASTInner::Integer(5)
-        }])
-    };
+pub struct App {
+    ast: CursorAST
+}
 
-    frame.render_widget(Line::from(ast.render()), frame.area());
+impl App {
+
+    fn run_app<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> io::Result<bool> {
+        loop {
+            terminal.draw(|f| ui(f))?;
+    
+            if let Event::Key(key) = event::read()? {
+                if key.kind == event::KeyEventKind::Release {
+                    // Skip events that are not KeyEventKind::Press
+                    continue;
+                }
+
+                if key.code == KeyCode::Left {
+
+                }
+            }
+        }
+    }
+    
+    pub fn ui(&self, frame: &mut Frame) {
+        frame.render_widget(Line::from(self.ast.render()), frame.area());
+    }
 }
 
 // store the code as structured data in a file and have a custom editor to edit this code
 // https://docs.rs/ratatui-core/0.1.0-alpha.2/ratatui_core/text/struct.Span.html
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
     enable_raw_mode()?;
-    let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
+    let mut stderr = io::stderr();
     execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stderr);
     let mut terminal = Terminal::new(backend)?;
 
-    // create app and run it
-    let res = run_app(&mut terminal);
+    let mut app = App {
+        ast: CursorAST {
+            cursor: false,
+            inner: CursorASTInner::Add(vec![CursorAST {
+                cursor: true,
+                inner: CursorASTInner::Integer(5)
+            }])
+        }
+    };
+    let res = app.run_app(&mut terminal);
 
-    // restore terminal
     disable_raw_mode()?;
     execute!(
         terminal.backend_mut(),
@@ -78,25 +100,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     )?;
     terminal.show_cursor()?;
 
-    if let Ok(do_print) = res {
-        
-    } else if let Err(err) = res {
-        println!("{err:?}");
-    }
-
     Ok(())
-}
-
-fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<bool> {
-    loop {
-        terminal.draw(|f| ui(f))?;
-
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Release {
-                // Skip events that are not KeyEventKind::Press
-                continue;
-            }
-            return Ok(true);
-        }
-    }
 }
