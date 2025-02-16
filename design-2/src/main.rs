a07e9b31902f50b5e93e5986d307a5744b6977e9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-02-16 17:27:47 +0100

    format

--- design-2/src/main.rs
+++ design-2/src/main.rs
@@ -2,23 +2,27 @@ use std::{error::Error, io};
 
 use crossterm::event::KeyModifiers;
 use ratatui::{
-    backend::{Backend, CrosstermBackend}, crossterm::{
+    Frame, Terminal,
+    backend::{Backend, CrosstermBackend},
+    crossterm::{
         event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
         execute,
-        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    }, style::{Color, Style}, text::{Line, Span}, Frame, Terminal
+        terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+    },
+    style::{Color, Style},
+    text::{Line, Span},
 };
 
 pub enum ASTInner<T> {
     Integer(u64),
     Double(f64),
     Add(Vec<AST<T>>),
-    Multiply(Vec<AST<T>>)
+    Multiply(Vec<AST<T>>),
 }
 
 pub struct AST<T> {
     auxiliary: T,
-    inner: ASTInner<T>
+    inner: ASTInner<T>,
 }
 
 impl AST<bool> {
@@ -26,73 +30,105 @@ impl AST<bool> {
         let style = if self.auxiliary {
             Style::new().fg(Color::Black).bg(Color::White)
         } else {
-            Style::new().fg(Color::White).bg(Color::Black)
+            Style::new().fg(Color::White)
         };
         match &self.inner {
             ASTInner::Integer(value) => vec![Span::styled(value.to_string(), style)],
             ASTInner::Double(value) => vec![Span::styled(value.to_string(), style)],
-            ASTInner::Add(asts) => std::iter::once(Span::styled("(+", style)).chain(asts.iter().flat_map(|a| std::iter::once(Span::raw(" ")).chain(a.render()))).chain(std::iter::once(Span::styled(")", style))).collect(),
-            ASTInner::Multiply(asts) => std::iter::once(Span::styled("(* ", style)).chain(asts.iter().flat_map(|a| std::iter::once(Span::raw(" ")).chain(a.render()))).chain(std::iter::once(Span::styled(")", style))).collect(),
+            ASTInner::Add(asts) => std::iter::once(Span::styled("(+", style))
+                .chain(
+                    asts.iter()
+                        .flat_map(|a| std::iter::once(Span::raw(" ")).chain(a.render())),
+                )
+                .chain(std::iter::once(Span::styled(")", style)))
+                .collect(),
+            ASTInner::Multiply(asts) => std::iter::once(Span::styled("(* ", style))
+                .chain(
+                    asts.iter()
+                        .flat_map(|a| std::iter::once(Span::raw(" ")).chain(a.render())),
+                )
+                .chain(std::iter::once(Span::styled(")", style)))
+                .collect(),
         }
     }
 
     pub fn left(&mut self) {
         match &mut self.inner {
-            ASTInner::Integer(_) => {},
-            ASTInner::Double(_) => {},
+            ASTInner::Integer(_) => {}
+            ASTInner::Double(_) => {}
             ASTInner::Add(asts) => {
                 for i in 1..asts.len() {
                     if asts[i].auxiliary {
                         asts[i].auxiliary = false;
-                        asts[i-1].auxiliary = true;
+                        asts[i - 1].auxiliary = true;
                     }
                 }
-            },
+            }
             ASTInner::Multiply(asts) => {
                 for i in 1..asts.len() {
                     if asts[i].auxiliary {
                         asts[i].auxiliary = false;
-                        asts[i-1].auxiliary = true;
+                        asts[i - 1].auxiliary = true;
                     }
                 }
-            },
+            }
         }
     }
-    
+
     pub fn right(&mut self) {
         match &mut self.inner {
-            ASTInner::Integer(_) => {},
-            ASTInner::Double(_) => {},
+            ASTInner::Integer(_) => {}
+            ASTInner::Double(_) => {}
             ASTInner::Add(asts) => {
-                for i in (0..asts.len()-1).rev() {
+                for i in (0..asts.len() - 1).rev() {
                     if asts[i].auxiliary {
                         asts[i].auxiliary = false;
-                        asts[i+1].auxiliary = true;
+                        asts[i + 1].auxiliary = true;
                     }
                 }
-            },
+            }
             ASTInner::Multiply(asts) => {
-                for i in (0..asts.len()-1).rev() {
+                for i in (0..asts.len() - 1).rev() {
                     if asts[i].auxiliary {
                         asts[i].auxiliary = false;
-                        asts[i+1].auxiliary = true;
+                        asts[i + 1].auxiliary = true;
                     }
                 }
-            },
+            }
+        }
+    }
+
+    pub fn delete_left(&mut self) {
+        match &mut self.inner {
+            ASTInner::Integer(_) => {}
+            ASTInner::Double(_) => {}
+            ASTInner::Add(asts) => {
+                for i in (0..asts.len()).rev() {
+                    if asts[i].auxiliary {
+                        asts.remove(i);
+                    }
+                }
+            }
+            ASTInner::Multiply(asts) => {
+                for i in (0..asts.len()).rev() {
+                    if asts[i].auxiliary {
+                        asts.remove(i);
+                    }
+                }
+            }
         }
     }
 }
 
 pub struct App {
-    ast: AST<bool>
+    ast: AST<bool>,
 }
 
 impl App {
-
     fn run_app<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> io::Result<()> {
         loop {
             terminal.draw(|f| self.ui(f))?;
-    
+
             if let Event::Key(key) = event::read()? {
                 if key.kind == event::KeyEventKind::Release {
                     // Skip events that are not KeyEventKind::Press
@@ -100,7 +136,7 @@ impl App {
                 }
                 match key.code {
                     KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => {
-                        return Ok(())
+                        return Ok(());
                     }
                     KeyCode::Left => {
                         self.ast.left();
@@ -108,12 +144,19 @@ impl App {
                     KeyCode::Right => {
                         self.ast.right();
                     }
+                    KeyCode::Backspace => {
+                        self.ast.delete_left();
+                    }
+                    KeyCode::Delete => {
+                        self.ast.delete_right();
+                    }
+                    KeyCode::Char('i') => {}
                     _ => {}
                 }
             }
         }
     }
-    
+
     pub fn ui(&self, frame: &mut Frame) {
         frame.render_widget(Line::from(self.ast.render()), frame.area());
     }
@@ -132,17 +175,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut app = App {
         ast: AST {
             auxiliary: false,
-            inner: ASTInner::Add(vec![AST {
-                auxiliary: false,
-                inner: ASTInner::Integer(5)
-            }, AST {
-                auxiliary: false,
-                inner: ASTInner::Integer(6)
-            }, AST {
-                auxiliary: true,
-                inner: ASTInner::Integer(7)
-            }])
-        }
+            inner: ASTInner::Add(vec![
+                AST {
+                    auxiliary: false,
+                    inner: ASTInner::Integer(5),
+                },
+                AST {
+                    auxiliary: false,
+                    inner: ASTInner::Integer(6),
+                },
+                AST {
+                    auxiliary: true,
+                    inner: ASTInner::Integer(7),
+                },
+            ]),
+        },
     };
     let res = app.run_app(&mut terminal);
 
