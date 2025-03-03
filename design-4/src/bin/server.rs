14664a1664d2bc5ace4984b3e8f5713e5898178d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-03-03 22:10:56 +0100

    wip

--- design-4/src/bin/server.rs
+++ design-4/src/bin/server.rs
@@ -1,24 +1,44 @@
-use tokio::{io::AsyncReadExt as _, net::TcpListener, spawn};
+use std::sync::Arc;
+
+use append_only_vec::AppendOnlyVec;
+use tokio::{
+    io::{AsyncReadExt as _, AsyncWriteExt as _},
+    net::TcpListener,
+    spawn,
+    sync::watch,
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:1234").await.unwrap();
 
-    let streams = Vec::new();
+    let history: Arc<AppendOnlyVec<Vec<u8>>> = Arc::new(AppendOnlyVec::new());
+
+    let (tx, mut rx) = watch::channel(history.len());
 
     while let Ok((mut stream, addr)) = listener.accept().await {
         let (mut read, mut write) = stream.into_split();
 
-        spawn(async move {
-            while let Ok(rec) = receive_receiver.recv().await {
-                if rec.1 == addr {
-                    continue;
+        {
+            let mut rx = rx.clone();
+            let history = history.clone();
+            spawn(async move {
+                let mut last_index = 0;
+                loop {
+                    let current_index = *rx.borrow_and_update();
+                    while last_index < current_index {
+                        let val = &history[last_index];
+                        write.write(&val.len().to_be_bytes()).await.unwrap();
+                        write.write(&val).await.unwrap();
+                        last_index += 1;
+                    }
+                    if rx.changed().await.is_err() {
+                        break;
+                    }
                 }
-                let len: u64 = serialized.as_bytes().len().try_into().unwrap();
-                write.write(&len.to_be_bytes()).await.unwrap();
-                write.write(serialized.as_bytes()).await.unwrap();
-            }
-        });
+            });
+        }
+        let history = history.clone();
         spawn(async move {
             loop {
                 let mut buf: [u8; 8] = [0; 8];
@@ -27,7 +47,7 @@ async fn main() -> std::io::Result<()> {
                 let mut buf = vec![0; size.try_into().unwrap()];
                 read.read_exact(&mut buf).await.unwrap();
 
-                receive_sender.send((deserialized, addr)).unwrap();
+                history.push(buf);
             }
         });
     }
